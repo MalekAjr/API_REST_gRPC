7d6648cc3bcf128dcc6ba4ebf8063f6785eeefa0
MalekAjroud <malek.ajroud@polytechnicient.tn>
2023-05-24 14:52:59 +0100

    Initial commit

---
+++ data/statu.js
@@ -0,0 +1,19 @@
+const PROTO_PATH = "./grpc/statu.proto";
+
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true,
+});
+
+const statuProto = grpc.loadPackageDefinition(packageDefinition).statu;
+const statu = new statuProto.StatusService(
+  "localhost:30045",
+  grpc.credentials.createInsecure()
+);
+
+module.exports = statu;
